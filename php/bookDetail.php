72c09393f5ad7a3018e1e7d0e8bad90955dd59ac
sebastiankny <50146346+sebastiankny@users.noreply.github.com>
2019-12-15 18:56:52 +0800

    see alter payment table

--- php/bookDetail.php
+++ php/bookDetail.php
@@ -1,6 +1,5 @@
-<!-- <?php
-session_start();?> -->
 <?php
+session_start();
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 $conn = new mysqli("localhost", "root", "", "brsp");
 $sql="SELECT bookname,genre,author,publisher,ROUND(price,2) AS price,rent_rate,bookownerid,url FROM book WHERE bookid = '".$_GET["x"]."'";
@@ -16,6 +15,8 @@ $data = $result->fetch_assoc();
 // echo "<br>bookid: ".$data["bookname"];
 $conn->close();
 ?>
+
+
 <html>
 <head>
 	<meta charset="utf-8">
@@ -36,33 +37,14 @@ echo "Favorite animal is " . $_SESSION["favanimal"] . ".";
 		Price: RM<?php echo $data["price"] ?><br>
 	</div>
 
+	<div>
+    <form id="myForm" action="" method="GET">
+      <input id="data" type="text" name="x" value="">
+      <br><br>
+      <input type="submit" value="Submit">
+    </form>
+  </div>
+
+  
 </body>
 </html>
-<!-- ajax -->
-<!-- <script>
-var obj, xmlhttp, myObj, x, txt = "";
-
-obj = {"bookname":"book2"};
-// console.log(obj);
-dbParam = JSON.stringify(obj);
-xmlhttp = new XMLHttpRequest();
-xmlhttp.onreadystatechange = function() {
-  if (this.readyState == 4 && this.status == 200) {
-  	console.log(this.responseText);
-    myObj = JSON.parse(this.responseText);
-    console.log(myObj);
-    // document.getElementById("demo").innerHTML = txt;
-    //book1
-  	// document.getElementById("bookname").innerHTML=myObj[0].bookname;
-   //  document.getElementById("price").innerHTML="Price: RM"+myObj[0].price;
-   //  document.getElementById("img").src=myObj[0].url;
-   //  document.getElementById("author").innerHTML=myObj[0].url;
-
-  }
-};
-xmlhttp.open("POST", "bookDetail.php", true);
-xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-// xmlhttp.send();
-xmlhttp.send("x=" + dbParam);
-</script>
- -->
